d02938407aa45f1b4b1f188d0e628d7f2dcba643
bryanS6 <129802677+bryanS6@users.noreply.github.com>
2025-09-06 12:24:12 -0300

    Add files via upload

---
+++ Faixadeconsumo.py
@@ -0,0 +1,29 @@
+print("MEDIDOR DE CONSUMO")
+
+marca = input("forneça a marca do veiculo: ")
+modelo = input("forneça o modelo do veiculo:")
+km = int(input("km's percorridos: "))
+lts = int(input("quantidade de litros gastos: "))
+
+consumo = km / lts
+
+if consumo < 6:
+    FdC = ("Altissimo consumo")
+    print(f"{marca} {modelo} fez {consumo} km/L e isso é considerado um {FdC}")
+elif consumo > 6 and consumo <= 9:
+    FdC = ("Alto consumo")
+    print(f"{marca} {modelo} fez {consumo} km/L e isso é considerado um {FdC}")
+elif consumo > 9 and  consumo <= 12:
+    FdC = ("Médio consumo")
+    print(f"{marca} {modelo} fez {consumo} km/L e isso é considerado um {FdC}")
+elif consumo > 12 and consumo <= 15:
+    FdC = ("Bom consumo")
+    print(f"{marca} {modelo} fez {consumo} km/L e isso é considerado um {FdC}")
+elif consumo > 15:
+    FdC = ("Ótimo consumo")
+    print(f"{marca} {modelo} fez {consumo} km/L e isso é considerado um {FdC}")
+    
+
+
+    
+
